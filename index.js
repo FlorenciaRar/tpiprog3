28ba5ef66fb3eaa7377229e43ef0e7049c1612b6
FlorenciaRar <florenciastrugo@gmail.com>
2024-10-06 18:18:06 -0300

    Cambio index + routes reclamos

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
+import { router as v1ReclamosEstadoRouter } from "./src/v1/routes/reclamosEstadoRoutes.js";
+import { router as v1ReclamosTipoRouter } from "./src/v1/routes/reclamosTipoRoutes.js";
+import { router as v1OficinasRouter } from "./src/v1/routes/oficinasRoutes.js";
+import { router as v1ReclamosRouter } from "./src/v1/routes/reclamosRoutes.js";
+import { router as v1UsuariosRouter } from "./src/v1/routes/usuariosRoutes.js";
+import { swaggerConfig } from "./src/config/swaggerConfig.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+app.use(helmet());
+
+const swaggerOptions = swaggerConfig;
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+app.use('/documentacion', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.get("/", (req, res) => {
+  res.json({ estado: true });
+});
+
+app.use("/api/v1/reclamos-estado", v1ReclamosEstadoRouter);
+app.use("/api/v1/reclamos-tipo", v1ReclamosTipoRouter);
+app.use("/api/v1/oficinas", v1OficinasRouter);
+app.use("/api/v1/reclamos", v1ReclamosRouter);
+app.use("/api/v1/usuarios", v1UsuariosRouter);
+
+const puerto = process.env.PUERTO;
+app.listen(puerto, () => {
+  console.log(`Estoy escuchando en ${puerto}`);
+});
--- src/v1/routes/reclamosRoutes.js
+++ src/v1/routes/reclamosRoutes.js
@@ -5,45 +5,22 @@ const router = express.Router();
 
 const reclamosController = new ReclamosController();
 
-router.get("/", reclamosController.buscarTodos);
+router.get("/", reclamosController.buscarTodos); 
 router.get("/:idReclamo", reclamosController.buscarId);
-router.post("/", reclamosController.crear);
-router.patch("/:idReclamo", reclamosController.modificar);
+router.post("/", reclamosController.crear); // Autenticar
+router.patch("/:idReclamo", reclamosController.modificar); //Autenticar
+
+// Cancelar un reclamo (para clientes)
 router.patch("/:idReclamo/cancelar", reclamosController.cancelar) // Autenticar
 
-// Finalizar reclamo /:idReclamo/finalizar
+// Cambiar estado (para empleados)
+router.patch("/:idReclamo/estado/:idReclamoEstado", reclamosController.cambiarEstado) // Autenticar
 
-// Reclamos por usuario /reclamos/usuario
+// Reclamos por usuario
+router.get("/usuario/:idUsuario", reclamosController.buscarUsuario); // Temporalmente con parametro
 
+// Reclamos por oficina
 router.get("/oficina/:idUsuario", reclamosController.buscarOficina); // Temporalmente con parametro
-// Swagger
 
-/**
- * @swagger
- * /api/reclamos:
- *   get:
- *     summary: Obtiene una lista de todos los reclamos
- *     tags: [Actors]
- *     responses:
- *       200:
- *         description: Lista de reclamos
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Reclamo'
- *             examples:
- *               ejemplo1:
- *                 summary: Ejemplo de respuesta exitosa
- *                 value:
- *                   - actorId: 1
- *                     firstName: PENELOPE
- *                     lastName: GUINESS
- *                     lastUpdate: 2024-09-12 18:05:18
- *                   - actorId: 2
- *                     firstName: NICK
- *                     lastName: CAGE
- *                     lastUpdate: 2023-10-12 18:36:36
- */
+
 export { router };

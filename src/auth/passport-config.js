3d5207c9638473f51bbb000ed46123acf1dc89a1
Hukke1990 <gastonbordet@gmail.com>
2024-10-18 20:14:57 -0300

    login work

--- src/auth/passport-config.js
+++ src/auth/passport-config.js
@@ -3,7 +3,6 @@ import { Strategy as LocalStrategy } from "passport-local";
 import { conexion } from "../database/conexion.js";
 import crypto from "crypto";
 
-// Configuración de la estrategia local
 passport.use(
   new LocalStrategy(
     {
@@ -12,7 +11,8 @@ passport.use(
     },
     async (correo, contrasenia, done) => {
       try {
-        const sql = "SELECT * FROM usuarios WHERE correoElectronico = ? AND contrasenia = SHA2(?,256)";
+        const sql =
+          "SELECT nombre, apellido, correoElectronico, idUsuarioTipo FROM usuarios WHERE correoElectronico = ? AND contrasenia = SHA2(?,256)";
         const [result] = await conexion.query(sql, [correo, contrasenia]);
 
         if (result.length === 0) {
@@ -21,16 +21,7 @@ passport.use(
 
         const usuario = result[0];
 
-        // Verificar la contraseña usando SHA-2
-        const hash = crypto
-          .createHash("sha256")
-          .update(contrasenia)
-          .digest("hex");
-        if (hash === usuario.contrasenia) {
-          return done(null, usuario);
-        } else {
-          return done(null, false, { message: "Credenciales incorrectas" });
-        }
+        return done(null, usuario);
       } catch (err) {
         return done(err);
       }
@@ -38,22 +29,21 @@ passport.use(
   )
 );
 
-// Serializar y deserializar usuario
-passport.serializeUser((usuario, done) => {
-  done(null, usuario.idUsuario);
-});
+// passport.serializeUser((usuario, done) => {
+//   done(null, usuario.idUsuario);
+// });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const sql = "SELECT * FROM usuarios WHERE idUsuario = ?";
-    const [result] = await conexion.query(sql, [id]);
-    if (result.length === 0) {
-      return done(new Error("Usuario no encontrado"));
-    }
-    done(null, result[0]);
-  } catch (err) {
-    done(err);
-  }
-});
+// passport.deserializeUser(async (id, done) => {
+//   try {
+//     const sql = "SELECT * FROM usuarios WHERE idUsuario = ?";
+//     const [result] = await conexion.query(sql, [id]);
+//     if (result.length === 0) {
+//       return done(new Error("Usuario no encontrado"));
+//     }
+//     done(null, result[0]);
+//   } catch (err) {
+//     done(err);
+//   }
+// });
 
 export default passport;
